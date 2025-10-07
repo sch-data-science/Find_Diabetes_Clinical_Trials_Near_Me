e0327825fb52c47e06f250b639f0bc87cb5d315f
SCH Data Science Outreach <andrew.cooper@seattlechildrens.org>
2025-10-06 21:13:54 -0700

    Add files via upload
    update the readme tab with link to SCH clinical trails and research opportunities page

--- app.R
+++ app.R
@@ -13,21 +13,32 @@ library(shinyBS)
 
 trials <- readRDS("trials.RDS")
 
+#DIABETES
 
 
 SiteStatus <- c(sort(unique(trials$STATUS)))
 Phase <- c("Phase 1", "Phase 2", "Phase 3", "Phase 4") 
+Condition <- c("Type1","Type2","Gestational","Prediabetes","Neuropathy","Hyperglycemia","Hypoglycemia","Hypertension")
+names(Condition) = c("Type 1 Diabetes","Type 2 Diabetes","Gestational Diabetes","Prediabetes","Neuropathic Complications","Hyperglycemia","Hypoglycemia","Hypertension")
 StudyState <- c(sort(unique(trials$STATE)))
 StudyCity <- c(sort(unique(trials$CITYSTATE)))
 
 ui <- fluidPage(
-
+  
+  tags$style(HTML(".dataTables_wrapper .dataTables_filter {
+                     float: left;
+                     padding-left: 50px;}
+                  .dataTables_wrapper .dataTables_filter input{
+                      width: 500px;}"
+  )
+  ),
+  
   titlePanel(
     fluidRow(
       column(2, HTML('<a target="_blank" rel="noopener noreferrer" href="https://www.seattlechildrens.org/"><img src = "logo.jpg" height = 100></a>')),
-      column(10,HTML("Clinical Trials Near Me<br><small>Interventional studies in the United States of America with at least one site/location currently recuriting.<br>
+      column(10,HTML("Diabetes-related Clinical Trials Near Me<br><small>Interventional studies in the United States of America with at least one site/location currently recuriting.<br>
                      Data last refreshed ",
-                     format(as.Date(trials$LATEST_REFRESH[1]),format = "%m/%d/%Y"),"</small>"))),windowTitle="Clinical Trials Near Me"),
+                     format(as.Date(trials$LATEST_REFRESH[1]),format = "%m/%d/%Y"),"</small>"))),windowTitle="Diabetes-related Clinical Trials Near Me"),
   
   # Create a new Row in the UI for selectInputs
   tabsetPanel(type="tabs",
@@ -98,8 +109,30 @@ ui <- fluidPage(
                                 
                          ),
                          column(3,
+                                
+                                bsTooltip("Condition_input_a",
+                                          "See Read Me tab for more information about the drop-down options",
+                                          placement = "right", trigger = "hover"),
+                                
+                                
+                                pickerInput("Condition_input", 
+                                            label=tagList(
+                                              "Condition Selector",
+                                              tags$span(
+                                                icon("question-circle"),
+                                                id = "Condition_input_a",
+                                                style = "margin-left: 5px; cursor: pointer; color: #1c9ed8;"
+                                              )
+                                            ),
+                                            Condition, selected=Condition,
+                                            multiple=TRUE,
+                                            options = list(
+                                              "title" = 'Click to see options',
+                                              'actions-box'= TRUE
+                                            )
+                                ),
                                 bsTooltip("Condition_search_a",
-                                          "Select the Clinical Trials whose Brief Title, Brief Summary, Keyword, and Conditions information contains ALL the words entered below",
+                                          "Select the Clinical Trials whose Brief Title, Brief Summary, Keyword, and Conditions information, when combined, contain ALL the words entered below",
                                           placement = "right", trigger = "hover"),
                                 
                                 textInput("Condition_search",
@@ -112,7 +145,7 @@ ui <- fluidPage(
                                               style = "margin-left: 5px; cursor: pointer; color: #1c9ed8;"
                                             )
                                           ))
-                                
+ 
                          ),
                          column(2,
                                 pickerInput("State_input", "State Select",
@@ -133,7 +166,7 @@ ui <- fluidPage(
                                 ),
                                 
                          ),
-                         column(5,HTML("<b>Select broadest State and Cities to the left, then Drag and Zoom to further refine your search</b>")),
+                         column(5,HTML("<b>Drag and Zoom to further refine your search</b>")),
                          column(5,leafletOutput('map'))
                        ),
                        
@@ -158,16 +191,26 @@ ui <- fluidPage(
                             Geocoding provided by <a target='_blank' rel='noopener noreferrer' href='https://docs.mapbox.com/api/search/geocoding/'>Mapbox Geocoding API</a>. <br>To contact Mapbox and OpenStreetMap to suggest improvements to the map, itself, <strong><a href='https://apps.mapbox.com/feedback/' target='_blank'>Please click here</a></strong>"),
                        HTML("<br>"),
                        HTML("<br>"),
+                       HTML("The customized Condition Selector searches Brief Summary, Keyword, and Conditions fields in the data provided by ClinicalTrials.gov for the following combinations.  When more than one option is checked, Clinical Trials are filtered to include ANY trials that contain the options.
+                       Please see the <a href = 'https://github.com/sch-data-science/Find_Diabetes_Clinical_Trials_Near_Me/blob/main/getdata.R'>code repository</a> for the actual coding statements. <br>
+                       
+                       <ul>
+                            <li> 'Type 1 Diabetes' looks for various permuations of 'type' and '1' or 'I', with or without spaces or hyphens, along with 't1d' or 't1dm', and with various capitalizaitons.</li>
+                            <li> 'Type 2 Diabetes' looks for various permuations of 'type' and '2' or 'II', with or without spaces or hyphens, along with 't2d' or 't2dm', and with various capitalizaitons.</li>
+                            <li> 'Gestational Diabetes' looks for the words 'gestational' or 'gdn', with various capitalizations. </li>
+                            <li> 'Prediabetes' looks for the pattern 'prediabet' or the combination of 'pre' and 'diabet' with spaces or hyphens between them, with various capitalizations.  This allows us to detect permutations of both prediabetes and prediabetic.</li>
+                            <li> 'Neuropathic Complications' looks for the pattern 'neuropath' to capture both 'neuropathic' and neuropathy, along with the words 'foot', 'ulcer', 'weakness', or 'amputation', all with various capitalizations. </li>
+                            <li> 'Hyperglycemia' looks for the pattern 'hyperglycem' to capture both 'hyperglycemia' and 'hyperglycemic', with various capitalizations.</li>
+                            <li> 'Hypoglycemia' looks for the pattern 'hypoglycem' to capture both 'hypoglycemia' and 'hypoglycemic', with various capitalizations.</li>
+                            <li> 'Hypertension' looks for the word 'hypertension' with various capitalizations. </li>
+                          </ul>"),
+                       HTML("<br>"),
                        HTML("For technical questions about this page, please contact:  Andrew.Cooper@SeattleChildrens.Org"),
                        HTML("<br>"),
                        HTML("For information regarding Seattle Children's Hospital, please visit <a target='_blank' rel='noopener noreferrer' href='https://www.seattlechildrens.org/'>our web page</a>."),
                        HTML("<br>"),
                        HTML("<br>"),
-                       HTML("Code for this app can be downloaded or forked from the <a target='_blank' rel='noopener noreferrer' href='https://github.com/sch-data-science/'>Seattle Children's Hospital Data Science GitHub page</a>."),
-                       HTML("<br>"),
-                       HTML("An example that customizes this app for Diabetes-related clinical trials can be found <a target='_blank' rel='noopener noreferrer' href='https://schdatascience-find-diabetes-related-clinical-trials-near-me.share.connect.posit.cloud/'>HERE</a> with the associated code and directions for how to motify the app found on
-                            <a target='_blank' rel='noopener noreferrer' href='https://github.com/sch-data-science/'>Seattle Children's Hospital Data Science GitHub page</a>.")
-                       
+                       HTML("Code for this app can be downloaded or forked from the <a target='_blank' rel='noopener noreferrer' href='https://github.com/sch-data-science/'>Seattle Children's Hospital Data Science GitHub page</a>.")
               )))
 
 server <- function(input, output,session) {
@@ -194,11 +237,10 @@ server <- function(input, output,session) {
                    data$STATE %in% input$State_input &
                    data$CITYSTATE %in% input$City_input
                  ,]
-    data <- data %>% filter(input$Age_input >= MINAGE & input$Age_input<=MAXAGE)
     
-    # Try something like this paste(input$Phase_input, collapse = "|")) and add title search
+    data$NConditions = rowSums(data[,c("DUMMY","DUMMY",toupper(input$Condition_input))]/1)
+    data <- data %>% filter(NConditions>0 & input$Age_input >= MINAGE & input$Age_input<=MAXAGE)
     
-
     
     if(!(input$Condition_search %in% c(NULL, NA, " ",""))) {
       data <- data %>% filter(rowSums(sapply(
@@ -253,8 +295,13 @@ server <- function(input, output,session) {
   
   output$table <- DT::renderDataTable(DT::datatable({
     temp <- dataInBounds() 
-    temp <- temp %>% dplyr::select(ORG_STUDY_ID,BRIEFTITLE,FACILITYLOC,STATUS,PHASE,STUDYURL,AGE_RANGE,CONDITION)
-    names(temp) <- c("Study ID", "Study Title","Study Site","Site Status","Phase","Study URL", "Age Range","Condition(s)")
+    temp <- temp %>% dplyr::select(ORG_STUDY_ID,BRIEFTITLE,FACILITYLOC,STATUS,PHASE,STUDYURL,CONTACT,
+                                   #PRINCIPAL_INVESTIGATOR,
+                                   AGE_RANGE,CONDITION)
+    names(temp) <- c("Study ID", "Study Title","Study Site","Site Status","Phase","Study URL",
+                     "Contact Name | Email | Phone",
+                     #"Principal Investigator", 
+                     "Age Range","Condition(s)")
     temp
   }, escape = FALSE ,options = list(dom = 'ltp')
   
